b52fb8572cc07c9e5bd076edeaa0e97eead43884
unknown <icramzilruiz@gmail.com>
2025-09-16 11:24:00 +0800

    Working with UI. Adding Listbox and Labels for output.

--- Activity10-LibraryBookManagement/Form1.Designer.cs
+++ Activity10-LibraryBookManagement/Form1.Designer.cs
@@ -28,13 +28,76 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.labelBook = new System.Windows.Forms.Label();
+            this.labelAuthor = new System.Windows.Forms.Label();
+            this.labelStatus = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Items.AddRange(new object[] {
+            "C# Programming",
+            "The Lord of the Rings",
+            "Introduction to Algorithms",
+            "A Brief History of Time"});
+            this.listBox1.Location = new System.Drawing.Point(266, 76);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(227, 84);
+            this.listBox1.TabIndex = 0;
+            // 
+            // labelBook
+            // 
+            this.labelBook.AutoSize = true;
+            this.labelBook.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelBook.Location = new System.Drawing.Point(263, 199);
+            this.labelBook.Name = "labelBook";
+            this.labelBook.Size = new System.Drawing.Size(63, 20);
+            this.labelBook.TabIndex = 1;
+            this.labelBook.Text = "Book: ";
+            // 
+            // labelAuthor
+            // 
+            this.labelAuthor.AutoSize = true;
+            this.labelAuthor.Location = new System.Drawing.Point(264, 231);
+            this.labelAuthor.Name = "labelAuthor";
+            this.labelAuthor.Size = new System.Drawing.Size(48, 16);
+            this.labelAuthor.TabIndex = 2;
+            this.labelAuthor.Text = "Author:";
+            // 
+            // labelStatus
+            // 
+            this.labelStatus.AutoSize = true;
+            this.labelStatus.Location = new System.Drawing.Point(263, 262);
+            this.labelStatus.Name = "labelStatus";
+            this.labelStatus.Size = new System.Drawing.Size(47, 16);
+            this.labelStatus.TabIndex = 3;
+            this.labelStatus.Text = "Status:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Controls.Add(this.labelStatus);
+            this.Controls.Add(this.labelAuthor);
+            this.Controls.Add(this.labelBook);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
+            this.Text = "Library Book Management";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label labelBook;
+        private System.Windows.Forms.Label labelAuthor;
+        private System.Windows.Forms.Label labelStatus;
     }
 }
 
